fc49006c58ce2e50e0b2283629fa0e59f3087f52
Everardo J Barojas M <everardo@prescrypto.com>
2018-10-07 22:35:49 -0500

    Load bigquery librariy

--- ri5c/get_contract.py
+++ ri5c/get_contract.py
@@ -1,7 +1,11 @@
 # Let's get some data
+from google.cloud import bigquery
+bigquery_client = bigquery.Client()
+
 
 def get_contract(contract_address):
     # Connect
+    bq = bigquery.Client()
     # Collect
     # Save
     pass
