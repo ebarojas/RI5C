ab376b4bbfa4c4994cb6cd01eccd78731790e018
Everardo J Barojas M <everardo@prescrypto.com>
2018-12-19 20:50:01 -0600

    Add sample graph generation code

--- ri5c/get_contract.py
+++ ri5c/get_contract.py
@@ -1,6 +1,20 @@
+# -*- coding: utf-8 -*-
+
 # Let's get some data
 from google.cloud import bigquery
 bigquery_client = bigquery.Client()
+
+# Nodos = addresses
+# Aristas = proporcional a tamaño de transacción
+# Colour?
+# Distribución de grado – desviación estándar de distribución de grado
+# Cuál es el tipo de red
+# Teoría de redes
+# Centralidad de nodos [la más importante es el grado]
+# *Static or dynamic?*
+# Maybe use pandas
+
+# Do this https://cloud.google.com/bigquery/docs/parameterized-queries#bigquery-query-params-cli
 
 
 def get_contract(contract_address):
@@ -24,10 +38,13 @@ def get_contract(contract_address):
     iterator = query_job.result(timeout=30)
     rows = list(iterator)
 
+    # This returns a simple dataset that can be used and tested
+    return rows
     # Collect
     # How do I create the network? – how do i assign elements?  
     # Save
-    pass
+    
+    # pass
 
 
 # query_params = [
@@ -51,3 +68,39 @@ def get_contract(contract_address):
 
 # Good reference for BigQuery
 # https://cloud.google.com/bigquery/docs/parameterized-queries#bigquery-query-params-python
+
+
+# This is example code of a graph
+'''
+import sys
+
+import pandas as pd
+import networkx as nx
+# Important because no GUI in vagrant
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from ri5c.get_contract import get_contract
+
+G = nx.Graph()
+
+# Where r is the result of get_contract
+bct="0xf5839f46ed000d70cbab1fcd03e29e85f3aecd82"
+r = get_contract(bct)
+df = pd.DataFrame(data=[list(x.values()) for x in r], columns=list(r[0].keys()))
+
+nodes = df['to_address'].unique()
+G.add_nodes_from(nodes)
+
+for index, row in df.iterrows():
+    G.add_edge(row['to_address'], row['from_address'])
+
+# Our graph is G
+# Because there is no GUI, file needs to be saved
+import pylab
+nx.draw(G)
+pls.show()
+pylab.savefig('foo.png')
+
+'''
+
