11f4959cd906570e68c47baded738d13a2546221
Everardo J Barojas M <everardo@prescrypto.com>
2019-02-04 23:39:18 -0600

    Create different methods for creating weighted graph and drawing it.

--- ri5c/get_contract.py
+++ ri5c/get_contract.py
@@ -8,6 +8,11 @@ matplotlib.use('Agg') # Important because no GUI in vagrant or server
 import matplotlib.pyplot as plt
 import community # Detecting communities
 import pylab # Exporting figures
+# For log
+from math import *
+
+# Note to self: Interesting to consider using Gremlin for querying the databases
+# https://pypi.org/project/gremlinpython/
 
 def get_contract(contract_address, limit=1000):
     # Instantiate Big Query
@@ -59,6 +64,21 @@ def create_graph(contract):
     print(nx.info(G))
     return G
 
+# Created with F. Ramírez Alatriste – it's a weighted graph - 01.02.2019
+def create_graph_new(contract):
+    print "Creating a simple graph..."
+    G = nx.Graph()
+    # Pandas dataframe
+    df = pd.DataFrame(data=[list(x.values()) for x in contract], columns=list(contract[0].keys()))
+    # Graph
+    # import code; code.interact(local=locals())
+    # Somehow value is in log_index, this needs to be fixed WIP
+    # LOG INDEX IS A TEMP MEASURE, needs to be changed
+    G = nx.from_pandas_edgelist(df, source='from_address', target='to_address', edge_attr="log_index")
+    print "Graph created."
+    print(nx.info(G))
+    return G
+
 def draw_graph(graph, filename='network.png', x=100, y=100):
     print "Starting to draw..."
     G = graph
@@ -68,7 +88,7 @@ def draw_graph(graph, filename='network.png', x=100, y=100):
     #drawing
     size = float(len(set(partition.values())))
     pos = nx.spring_layout(G)
-    count = 0.
+    count = 0.0
     for com in set(partition.values()) :
         count = count + 1.
         list_nodes = [nodes for nodes in partition.keys()
@@ -82,6 +102,35 @@ def draw_graph(graph, filename='network.png', x=100, y=100):
     print "And now, let's save it in "+filename
     pylab.savefig('network.png')
 
+# WEIGHTED VERSION
+def draw_weighted_graph(graph, filename='network_weighted.png', x=500, y=500):
+    print "Starting to draw a weighted graph..."
+    G = graph
+
+    # import code; code.interact(local=locals())
+    node_degree_size=[G.degree(n) for n in G.nodes()] # This is what makes nodes larger
+
+    options = { 
+        'node_color': 'blue', 
+        'node_size': node_degree_size, 
+        'width': 1, # What does this do?
+        'arrowstyle': '-|>', 
+        'arrowsize': 12, # What does this do?
+    }
+
+    # import code; code.interact(local=locals())
+    # LOG INDEX IS A TEMP MEASURE, needs to be changed
+    weights = [0.1*log(float(i['log_index'])) for i in dict(G.edges).values()]
+    #print(weights)
+    #nx.draw(g,**options)
+    fig, ax = plt.subplots(figsize=(x,y))
+    pos = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G, pos, ax = ax, node_size=node_degree_size, labels=True)
+    nx.draw_networkx_edges(G, pos, width=weights, ax=ax)
+
+    print "And now, let's save it in "+filename
+    pylab.savefig('network_weighted.png')
+
 
 def network_this(contract, limit=1000):
     contract = get_contract(contract, limit)
@@ -90,7 +139,14 @@ def network_this(contract, limit=1000):
     print "Success"
     pass
 
-from ri5c.get_contract import network_this, get_contract, create_graph, draw_graph
+def network_this_w(contract, limit=1000):
+    contract = get_contract(contract, limit)
+    graph = create_graph_new(contract)
+    draw_weighted_graph(graph)
+    print "Success"
+    pass
+
+# from ri5c.get_contract import network_this, get_contract, create_graph, draw_graph
 # g = nx.from_pandas_edgelist(df, source='from_address', target='to_address',edge_attr="value")
 # Weighted example 
 # weights = [0.1*math.log(i['value']) for i in dict(g.edges).values()]
