2bf2930743446581f33ae8fe669d016f3da19e5e
Everardo J Barojas M <everardo@prescrypto.com>
2019-04-21 20:04:02 -0500

    Add tornado web server with simple HelloWorld [WIP]

---
+++ ri5c/views.py
@@ -0,0 +1,8 @@
+from tornado.web import RequestHandler
+
+class HelloWorld(RequestHandler):
+    """Print 'Hello, world!' as the response body."""
+
+    def get(self):
+        """Handle a GET request for saying Hello World!."""
+        self.write("Hello, world!")
