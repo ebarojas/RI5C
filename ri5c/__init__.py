349b10413fcac1a88af9acaae87e04063d015a39
Everardo J Barojas M <everardo@prescrypto.com>
2019-04-21 23:27:51 -0500

    Serve using Tornado and Python3 [WIP] - Still working on cleaning POST - No CSS syling whatsoever

--- ri5c/__init__.py
+++ ri5c/__init__.py
@@ -7,14 +7,14 @@ from tornado.httpserver import HTTPServer
 from tornado.options import define, options
 from tornado.web import Application
 from tornado.ioloop import IOLoop
-from ri5c.views import HelloWorld
+from ri5c.views import GraphView
 
 define('port', default=8000, help='port to listen on')
 
 def main():
     """Construct and serve the tornado application."""
     app = Application([
-        ('/', HelloWorld)
+        ('/', GraphView)
     ])
     http_server = HTTPServer(app)
     http_server.listen(options.port)
