87705830103d34442b0de25947f8dc60bd18ca44
Everardo J Barojas M <everardo@prescrypto.com>
2019-04-28 21:44:40 -0500

    Add Port

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ define('port', default=8000, help='port to listen on')
 def main():
     """Construct and serve the tornado application."""
     cwd = os.getcwd() # static files
+    port = int(os.environ.get("PORT", 8000))
 
     app = Application([
         (r'/', GraphView),
@@ -21,8 +22,8 @@ def main():
         (r'/(.*\.js)', StaticFileHandler, {"path": cwd} ),
     ])
     http_server = HTTPServer(app)
-    http_server.listen(options.port)
-    print('RI5C is listening on http://localhost:%i' % options.port)
+    http_server.listen(port)
+    print('RI5C is listening on http://localhost:%i' % port)
     IOLoop.current().start()
 
 if __name__ == "__main__":
